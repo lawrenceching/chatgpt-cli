f2663499b41f9364ef8bc7063923a6f9435336c7
Lawrence Ching <lawrence.ching.zh@gmail.com>
2023-03-27 14:31:31 +0800

    fixed the issue that app stopped when API call fail

--- index.mjs
+++ index.mjs
@@ -14,39 +14,45 @@ import prompts from 'prompts'
     let res = null
 
     while (true) {
-        const response = await prompts({
-            type: 'text',
-            name: 'value',
-            message: 'Question: '
-        });
 
-        const question = response.value;
+        try {
+            const response = await prompts({
+                type: 'text',
+                name: 'value',
+                message: 'Question: '
+            });
 
-        if (question === undefined) {
-            // User press Ctrl+C when prompted
-            break;
+            const question = response.value;
+
+            if (question === undefined) {
+                // User press Ctrl+C when prompted
+                break;
+            }
+
+            if (question.trim().length === 0) {
+                continue
+            }
+
+            if(res === null) {
+                res = await oraPromise(api.sendMessage(question), {
+                    text: question
+                })
+            } else {
+                res = await api.sendMessage(question, {
+                    conversationId: !!res ? res.conversationId : null,
+                    parentMessageId: !!res ? res.id : null,
+                })
+            }
+
+            if (debug)
+                console.log(res);
+            else
+                console.log(res.text);
+            console.log();
+        } catch (e) {
+            console.error(e);
         }
 
-        if (question.trim().length === 0) {
-            continue
-        }
-
-        if(res === null) {
-            res = await oraPromise(api.sendMessage(question), {
-                text: question
-            })
-        } else {
-            res = await api.sendMessage(question, {
-                conversationId: !!res ? res.conversationId : null,
-                parentMessageId: !!res ? res.id : null,
-            })
-        }
-
-        if (debug)
-            console.log(res);
-        else
-            console.log(res.text);
-        console.log();
     }
 
 })();
